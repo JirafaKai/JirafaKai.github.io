bb0d60824ae1d013640ff045f2771dd1ad8f6c1f
KKaiman <imkaiman214@gmail.com>
2015-03-19 00:17:23 +0800

    tr

--- wcp-test/object.js
+++ wcp-test/object.js
@@ -9,7 +9,7 @@ function myobj(){
 	}
 	this.getData = function(urlScript){
 		$.getJSON(urlScript, 
-			gogo(JData);
+			this.gogo(JData);
 		});
 	}
     function charAttr (charno, imgSrc, cname, ccname, cnName, phase, star, type, hp, hhp, sp, hsp, atk, hatk, def, hdef, cri, hcri, spr, ls1, ls2, as1, as1sp, as2, as2sp, ds1, ds2, ds3, spcComm, gamewith) {
