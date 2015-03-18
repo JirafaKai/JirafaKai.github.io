1bdd32d91e1a898a9e6a0d213680dc7e8ec7870d
KKaiman <imkaiman214@gmail.com>
2015-03-18 23:55:50 +0800

    try

--- wcp-test/object.js
+++ wcp-test/object.js
@@ -1,19 +1,18 @@
 function myobj(){
 	this.charactor=[];
 	this.commentG=[];
-	this.urlScript = "https://spreadsheets.google.com/feeds/list/1osCn09v241irWHcW2t21XxVjv41sSRMdb5rCduNG24I/2/public/values?alt=json";
-	this.urlScript2 = "https://spreadsheets.google.com/feeds/list/1osCn09v241irWHcW2t21XxVjv41sSRMdb5rCduNG24I/1/public/values?alt=json";
+	urlScript2 = "https://spreadsheets.google.com/feeds/list/1osCn09v241irWHcW2t21XxVjv41sSRMdb5rCduNG24I/1/public/values?alt=json";
 	
 	this.getTime = function(){
 		var Today = new Date();
 		return Today.getFullYear() + ' / ' + parseInt(Today.getMonth()+1) + ' / ' + Today.getDate();
 	}
-	this.getData = function(){
-		$.getJSON(this.urlScript, 
+	this.getData = function(urlScript){
+		$.getJSON(urlScript, 
 			function(JData){
 				for (var i in JData.feed.entry){
                     
-                    charactor[i] = new charAttr(
+                    this.charactor[i] = new charAttr(this.charactor[i]
 					  JData.feed.entry[i].gsx$charno.$t,
                       JData.feed.entry[i].gsx$imgsrc.$t,
                       JData.feed.entry[i].gsx$charname.$t,
@@ -50,4 +49,20 @@ function myobj(){
 			$("#overlay-loading").remove();
 		});
 	}
+	function charAttr (target,charno, imgSrc, cname, ccname, cnName, phase, star, type, hp, hhp, sp, hsp, atk, hatk, def, hdef, cri, hcri, spr, ls1, ls2, as1, as1sp, as2, as2sp, ds1, ds2, ds3, spcComm, gamewith) {
+	  target.charno=charno;
+      target.imgSrc=imgSrc;
+      target.cName=cname;
+      target.ccName=ccname;
+      target.cnName=cnName;
+      target.phase=phase;
+      target.star=star;
+      target.type=type;
+      target.lvMaxAttr={hp:hp, sp:sp, atk:atk, def:def, cri:cri, spr:spr};
+      target.hyperAttr={hp:hhp, sp:hsp, atk:hatk, def:hdef, cri:hcri};
+      target.sprComm=spcComm;
+      target.skill={ls:[ls1,ls2], as:[as1,as1sp,as2,as2sp], ds:[ds1,ds2,ds3]};
+      target.gamewith=gamewith;
+	  return target;
+    }
 }
