a31c8e490ccf88ebd5c158469784719cbc7a0eb3
bebe0314 <bellabel0314@gmail.com>
2015-04-21 09:36:02 +0800

    for sync only

---
+++ wcp/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+	<title>白貓Project</title>
+	<meta charset="utf-8">
+	<meta author="JirafaKai">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	
+	<link media="all" type="text/css" rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+<div class="nav-bar bg-blue-01">
+	<div class="wrapper-1024">
+		<div class="nav-brand font-white">白貓Project資訊站</div>
+		<div class="nav-item"><button class="button button-01 bg-blue-01">副本</button></div>
+		<div class="nav-item"><button class="button button-01 bg-blue-01">怪物</button></div>
+		<div class="nav-item"><button class="button button-01 bg-blue-01">武器</button></div>
+		<div class="nav-item"><button class="button button-01 bg-blue-01">角色</button></div>
+	</div>
+</div>
+
+<div class="index-section-1 bg-white">
+	<div class="wrapper-1024 position-r">
+		<div class="index-section-1-1">
+			<span class="title-01 font-blue-01 border-blue-01">站內更新</span>
+			<div class="char-icon-group">
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+			</div>
+			<div class="announce-group">
+				<div><a href="#">イベントキャラモチーフ武器ガチャに登場！</a></div>
+				<div><a href="#">『白猫』ゲーム内にしょこたんキャラが登場！</a></div>
+			</div>
+		</div>
+		<div class="index-section-1-2">
+			<span class="title-01 font-blue-01 border-blue-01">當期轉蛋</span>
+			<div class="icon-group">
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+				<a href="#"><img src="img/icon-41x41.png"/></a>
+			</div>
+			<div id="char-icon" class="icon-button bg-green-01"></div>
+			<div id="weapon-icon" class="icon-button bg-gray-01"></div>
+		</div>
+	</div>
+</div>
+
+<div class="section-2">
+	<div class="wrapper-1024 position-r">
+		
+	</div>
+</div>
+
+<div style="height:300px;" class="index-section-2 bg-blue-02">
+</div>
+
+</body>
+</html>
