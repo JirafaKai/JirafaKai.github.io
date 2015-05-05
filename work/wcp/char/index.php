e4b1b32e13188a0bea95a77397cc164ccc90bf3d
JirafaKai <bellabell0314@gmail.com>
2015-05-05 12:02:33 +0800

    Revert "char page half work"
    This reverts commit b7497a2df3fe4b621a7e3055267d2b22ea038957.

--- work/wcp/char/index.php
+++ work/wcp/char/index.php
@@ -1,195 +1,19 @@
 <?php include_once(__DIR__ . "/../assets/header.php"); ?>
 <!-- charactor show page -->
 
-<?php
-
-	function jobStandFor($job){
-		switch ($job){
-			case '劍':
-				return '劍士';
-			case '拳':
-				return '格鬥家';
-			case '斧':
-				return '戰士';
-			case '槍':
-				return '槍兵';
-			case '弓':
-				return '弓兵';
-			case '法':
-				return '魔導士';
-			case '雙刀':
-				return '雙刀';
-			default:
-				return '-';
-		}
-	}
-	
-	function setDSsEffect($row){
-		$dsArr = array($row['ps1'],$row['ps2'],$row['ps3']);
-		$dsPreArr = array(1,1,1,1,1);
-		$maxArr = array($row['hp100'], $row['sp100'], $row['atk100'], $row['def100'], $row['cri100']);
-		$hyperArr = array($row['hpHyper'], $row['spHyper'], $row['atkHyper'], $row['defHyper'], $row['criHyper']);
-		$changeFlag = array(0,0,0,0,0);
-		//echo '<br/>';
-		foreach ($dsArr as $i)
-		{
-			/* ========== HP ========== */
-			if (substr_count($i,'HP+10%')>0){
-					$dsPreArr[0]+=.10;
-			}
-			else if (substr_count($i,'HP+15%')>0){
-					$dsPreArr[0]+=.15;
-			}
-			else if (substr_count($i,'HP+20%')>0){
-					$dsPreArr[0]+=.20;
-			}
-			
-			/* ========== SP ========== */
-			if (substr_count($i,'SP+10%')>0){
-					$dsPreArr[1]+=.10;
-			}
-			else if (substr_count($i,'SP+15%')>0){
-					$dsPreArr[1]+=.15;
-			}
-			else if (substr_count($i,'SP+20%')>0){
-					$dsPreArr[1]+=.20;
-			}
-			
-			/* ========== ATK ========== */
-			if (substr_count($i,'攻擊+10%')>0){
-					$dsPreArr[2]+=.10;
-			}
-			else if (substr_count($i,'攻擊+15%')>0){
-					$dsPreArr[2]+=.15;
-			}
-			else if (substr_count($i,'攻擊+20%')>0){
-					$dsPreArr[2]+=.20;
-			}
-			
-			/* ========== DEF ========== */
-			if (substr_count($i,'防御+10%')>0){
-					$dsPreArr[3]+=.10;
-			}
-			else if (substr_count($i,'防禦+10%')>0){
-					$dsPreArr[3]+=.10;
-			}
-			else if (substr_count($i,'防御+15%')>0){
-					$dsPreArr[3]+=.15;
-			}
-			else if (substr_count($i,'防禦+15%')>0){
-					$dsPreArr[3]+=.15;
-			}
-			else if (substr_count($i,'防御+20%')>0){
-					$dsPreArr[3]+=.20;
-			}
-			else if (substr_count($i,'防禦+20%')>0){
-					$dsPreArr[3]+=.20;
-			}
-			
-			/* ========== CRI ========== */
-			if (substr_count($i,'會心+10%')>0){
-					$dsPreArr[4]+=.10;
-			}
-			else if (substr_count($i,'會心+15%')>0){
-					$dsPreArr[4]+=.15;
-			}
-			else if (substr_count($i,'會心+20%')>0){
-					$dsPreArr[4]+=.20;
-			}
-		}
-		
-		for ($i=0; $i<5; $i++)
-		{
-			$maxArr[$i]=$maxArr[$i]*$dsPreArr[$i];
-			if (substr_count($maxArr[$i], '.')>0)
-				$maxArr[$i]=floor($maxArr[$i]);
-			$hyperArr[$i]=$hyperArr[$i]*$dsPreArr[$i];
-			if (substr_count($hyperArr[$i], '.')>0)
-				$hyperArr[$i]=floor($hyperArr[$i]);
-			if ($dsPreArr[$i] > 1)
-				$changeFlag[$i] = 1;
-		}
-		//print_r($changeFlag);
-		
-		return array($changeFlag, $maxArr, $hyperArr);
-	}
-	
-	function printDSsEffect($arr, $key, $type){
-		if ($arr[0][0] == 1){
-			if ($key == 'max' && $type == 'hp')
-				echo ' ('.$arr[1][0].')';
-			if ($key == 'hyper' && $type == 'hp')
-				echo ' ('.$arr[2][0].')';
-		}
-		if ($arr[0][1] == 1){
-			if ($key == 'max' && $type == 'sp')
-				echo ' ('.$arr[1][1].')';
-			if ($key == 'hyper' && $type == 'sp')
-				echo ' ('.$arr[2][1].')';
-		}
-		if ($arr[0][2] == 1){
-			if ($key == 'max' && $type == 'atk')
-				echo ' ('.$arr[1][2].')';
-			if ($key == 'hyper' && $type == 'atk')
-				echo ' ('.$arr[2][2].')';
-		}
-		if ($arr[0][3] == 1){
-			if ($key == 'max' && $type == 'def')
-				echo ' ('.$arr[1][3].')';
-			if ($key == 'hyper' && $type == 'def')
-				echo ' ('.$arr[2][3].')';
-		}
-		if ($arr[0][4] == 1){
-			if ($key == 'max' && $type == 'cri')
-				echo ' ('.$arr[1][4].')';
-			if ($key == 'hyper' && $type == 'cri')
-				echo ' ('.$arr[2][4].')';
-		}
-	}
-	
-	
-	if (!empty($_GET['cno'])){
-		$cno = $_GET['cno'];
-		include('function/mysql_connect_user.php');
-		
-		$sql = "SELECT * FROM `char`,`charattrbase`,`charattr100`,`charattrhyper`,`passiveSkill` "
-				. "WHERE char.cNo = '" . $cno . "' "
-				. "AND charattrbase.cNo = char.cNo "
-				. "AND charattr100.cNo = char.cNo "
-				. "AND charattrhyper.cNo = char.cNo "
-				. "AND passiveSkill.cNo = char.cNo";
-		$result = mysql_query($sql) or die('MySQL query error');
-		
-		if (mysql_num_rows($result)==0)
-			echo 'No Data is founded.';
-		else{
-			$row = mysql_fetch_array($result);
-			echo $row['cNo'] . '<br/>';
-		}
-		$JName = explode('　', $row['JName']);
-		$CName = explode('　', $row['CName']);
-		$DSsEffect=setDSsEffect($row);
-	}
-	else 
-	{
-		echo 'Invaild request.';
-	}
-
-?>
-
-<div style="" class="content">
+<div style="display:none" class="content">
 	
 	<div class="char-show">
 		<div class="wrapper-1024 position-r">
 			<div class="leftside">
 				<div class="char-img bg-white">
-					<img style="display:block" class="img-2d" src="<?php echo $row['img2d'];?>"/>
-					<img style="display:none" class="img-3d" src="<?php echo $row['img3d'];?>"/>
-					<img style="display:none" class="img-arused" src="http://pic3.mofang.com/2014/0926/20140926061510799.jpg"/>
+					<img class="img-2d" src="img/haruka-2d.png"/>
+					<img class="img-3d" src="img/haruka-3d.png"/>
+					<img class="img-arused" src="http://pic3.mofang.com/2014/0926/20140926061510799.jpg"/>
 				</div>
 				<div class="img-button-group">
-					<div class="active" id="b2d">2D圖</div><div class="" id="b3d">
-					3D圖</div><div class="" id="barused">
+					<div class="active" id="b3d">3D圖</div><div class="" id="b2d">
+					2D圖</div><div class="" id="barused">
 					覺醒圖</div>
 				</div>			
 				<div class="char-comment position-r">
@@ -243,18 +67,18 @@
 					</div>
 					<div class="char-sub-attr">
 						<div class="char-Name bg-magician">
-							<span class="title"><?php echo $JName[0];?></span><br/>
-							<span class="name"><?php echo $JName[1];?></span>
+							<span class="title">白魔術見習生</span><br/>
+							<span class="name">ハルカ</span>
 						</div><div class="char-Name char-CName bg-magician">
-							<span class="title">台譯稱號</span><br/>
-							<span class="name">台譯名稱</span>
+							<span class="title">白魔術師見習生</span><br/>
+							<span class="name">春香</span>
 						</div>
 						<div class="char-sub-info">
 							<div class="char-phase bg-gray-02">
-								<?php echo $row['phase'];?>
+								第四期
 							</div>
 							<div class="char-job bg-gray-02">
-								<?php echo jobStandFor($row['job']);?>
+								魔法師
 							</div>
 						</div>
 						<div class="char-main-info">
@@ -263,7 +87,7 @@
 									型態
 								</div>
 								<div>
-									<?php echo $row['type'];?>
+									支援型
 								</div>
 							</div>
 							<div class="tag-of-2">
@@ -271,7 +95,7 @@
 									稀有度
 								</div>
 								<div>
-									<?php echo $row['star'].'星';?>
+									4星
 								</div>
 							</div>
 							<div class="tag-of-2">
@@ -279,7 +103,7 @@
 									COST<br/>(覺醒前/後)
 								</div>
 								<div>
-									<?php echo $row['costBefore'].'/'.$row['costAfter'];?>
+									9/11
 								</div>
 							</div>
 							<div class="tag-of-2">
@@ -287,7 +111,7 @@
 									配音
 								</div>
 								<div>
-									<?php echo $row['CV'];?>
+									大空直美
 								</div>
 							</div>
 						</div>
@@ -303,27 +127,27 @@
 							</tr>
 							<tr>
 								<td class="title">初期</td>
-								<td><?php echo $row['hpBase'];?></td>
-								<td><?php echo $row['spBase'];?></td>
-								<td><?php echo $row['atkBase'];?></td>
-								<td><?php echo $row['defBase'];?></td>
-								<td><?php echo $row['criBase'];?></td>
+								<td>131(242)</td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
 							</tr>
 							<tr>
 								<td class="title">Lv.100</td>
-								<td><?php echo $row['hp100'];?><?php printDSsEffect($DSsEffect,'max','hp')?></td>
-								<td><?php echo $row['sp100'];?><?php printDSsEffect($DSsEffect,'max','sp')?></td>
-								<td><?php echo $row['atk100'];?><?php printDSsEffect($DSsEffect,'max','atk')?></td>
-								<td><?php echo $row['def100'];?><?php printDSsEffect($DSsEffect,'max','def')?></td>
-								<td><?php echo $row['cri100'];?><?php printDSsEffect($DSsEffect,'max','cri')?></td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
 							</tr>
 							<tr>
 								<td class="title">Lv.100四突</td>
-								<td><?php echo $row['hpHyper'];?><?php printDSsEffect($DSsEffect,'hyper','hp')?></td>
-								<td><?php echo $row['spHyper'];?><?php printDSsEffect($DSsEffect,'hyper','sp')?></td>
-								<td><?php echo $row['atkHyper'];?><?php printDSsEffect($DSsEffect,'hyper','atk')?></td>
-								<td><?php echo $row['defHyper'];?><?php printDSsEffect($DSsEffect,'hyper','def')?></td>
-								<td><?php echo $row['criHyper'];?><?php printDSsEffect($DSsEffect,'hyper','cri')?></td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
+								<td>131</td>
 							</tr>
 						</table>
 						</div>
@@ -345,9 +169,9 @@
 								被動技能
 							</div>
 							<div class="ps-content">
-								<?php echo $row['ps1'];?><br/>
-								<?php echo $row['ps2'];?><br/>
-								<?php echo $row['ps3'];?>
+								HP+15%<br/>
+								技能傷害+15%<br/>
+								主動技能傷害+15%
 							</div>
 						</div>
 						<div class="active-skill">
@@ -478,7 +302,25 @@
 		</div>
 	</div> <!-- char-show -->
 </div> <!-- conetent-->
-<script type="text/javascript" src="char/function/function.js"></script>
+
+<?php
+	if (!empty($_GET['cno'])){
+		$cno = $_GET['cno'];
+		include('function/mysql_connect_user.php');
+		
+		$sql = "SELECT * FROM `char` WHERE `cno` = $cno;";
+		$result = mysql_query($sql) or die('MySQL query error');
+		
+		$row = mysql_fetch_array($result);
+		echo $row['cNo'] . '<br/>';
+		
+	}
+	else 
+	{
+		echo 'Invaild request.';
+	}
+
+?>
 
 
 <?php include_once(__DIR__ . "/../assets/footer.php"); ?>
